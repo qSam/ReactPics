91a24f4ae96bbc304e69cafa4ea30728c5611cc4
Qasim Laghari <go.jets.laghari@gmail.com>
2020-04-26 16:35:04 -0700

    Set state on response from search

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,12 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
+
+    state = {
+        images: []
+    };
     
-    async onSearchSubmit(term) {
+    onSearchSubmit = async term => {
         let response = await axios.get('https://api.unsplash.com/search/photos',{
         params: {
             query: term
@@ -14,13 +18,16 @@ class App extends React.Component {
                 Authorization: 'Client-ID 3bvy-aP8Fd-0tnLF8zlnYfw3T3IYqjbKUAkS3HBKGYs'
             }
         });
-        console.log(response.data.results);
+        this.setState({
+            images: response.data.results
+        });
     }
   
     render() {  
     return (
     <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
     </div>
     );
   }
