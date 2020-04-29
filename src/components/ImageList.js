03efad25afc673bb30aef636390b126c5711099b
Qasim Laghari <go.jets.laghari@gmail.com>
2020-04-28 18:32:45 -0700

    Add key and alt property

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 let ImageList = (props) => {
     let images = props.images.map((image)=> {
-        return <img src={image.urls.regular} />;
+        return <img alt={image.description} key={image.id} src={image.urls.regular} />;
     });
     return <div>{images}</div>;
 }
