3f4b3b23359dc237a1705f64fd28ba48fe8cbccc
Qasim Laghari <go.jets.laghari@gmail.com>
2020-04-30 09:08:36 -0700

    Get height from image DOM

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+    
     render() {
         return (
             <div>
                 <img 
+                    ref ={this.imageRef}
                     alt={this.props.image.description} 
                     src={this.props.image.urls.regular}    
                 />
